1223f8323d9b9297bd0060bc90b37225ca41177f
Galih Muhammad <galih0muhammad@gmail.com>
2017-11-18 16:46:11 +0700

    Enable synchronous chat loop between host and guest

--- lib/main.go
+++ lib/main.go
@@ -27,6 +27,13 @@ func RunHost(ip string) {
 
     fmt.Println("New connection accepted")
 
+    for {
+        handleHost(conn)
+    }
+
+}
+
+func handleHost(conn net.Conn) {
     reader := bufio.NewReader(conn)
     message, readErr := reader.ReadString('\n')
     if readErr != nil {
@@ -34,6 +41,16 @@ func RunHost(ip string) {
     }
 
     fmt.Println("message received: ", message)
+
+    // When getting reply from guest
+    fmt.Print("Send message: ")
+    replyReader := bufio.NewReader(os.Stdin)
+    replyMessage, replyErr := replyReader.ReadString('\n')
+    if replyErr != nil {
+      log.Fatal("Error: ", replyErr)
+    }
+
+    fmt.Fprint(conn, replyMessage)
 }
 
 // RunGuest takes an argument of ip address string as the destination to send message
@@ -44,6 +61,13 @@ func RunGuest(ip string) {
         log.Fatal("Error: ", dialErr)
     }
 
+    for {
+      handleGuest(conn)
+    }
+
+}
+
+func handleGuest(conn net.Conn) {
     fmt.Print("Send message: ")
     reader := bufio.NewReader(os.Stdin)
     message, readErr := reader.ReadString('\n')
@@ -52,4 +76,12 @@ func RunGuest(ip string) {
     }
 
     fmt.Fprint(conn, message)
+
+    replyReader := bufio.NewReader(conn)
+    replyMessage, replyErr := replyReader.ReadString('\n')
+    if replyErr != nil {
+        log.Fatal("Error: ", replyErr)
+    }
+
+    fmt.Println("message received: ", replyMessage)
 }
